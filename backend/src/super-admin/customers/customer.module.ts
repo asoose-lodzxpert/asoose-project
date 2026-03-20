d391bdeee01786edae710644e56505614bb7ba83
Dev Enoch <arhyelphilip024@gmail.com>
2026-03-20 07:17:40 +0100

    some fix

--- backend/src/super-admin/customers/customer.module.ts
+++ backend/src/super-admin/customers/customer.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../../users/users.module';
 import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
+import { ResendService } from 'src/mail/resend.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [CustomersController],
-  providers: [CustomersService],
+  providers: [CustomersService, ResendService],
 })
 export class CustomerModule {}
